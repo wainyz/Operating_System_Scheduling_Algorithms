8da7b04e3fadf30a85b9e5c7cdd1f691ff166358
wainyz <126802333+wainyz@users.noreply.github.com>
2023-11-04 14:08:08 +0800

    add author information

--- src/main/java/group/model/ProcessScheduleFrame.java
+++ src/main/java/group/model/ProcessScheduleFrame.java
@@ -267,11 +267,12 @@ public class ProcessScheduleFrame extends JFrame {
         //======== panel1 ========
         {
             panel1.setBorder(new TitledBorder("\u64cd\u4f5c\u7a7a\u95f4"));
-            panel1.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(
-            0,0,0,0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder
-            .BOTTOM,new java.awt.Font("Dia\u006cog",java.awt.Font.BOLD,12),java.awt.Color.
-            red),panel1. getBorder()));panel1. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.
-            beans.PropertyChangeEvent e){if("\u0062ord\u0065r".equals(e.getPropertyName()))throw new RuntimeException();}});
+            panel1.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border
+            . EmptyBorder( 0, 0, 0, 0) , "作者: wainyz", javax. swing. border. TitledBorder. CENTER, javax
+            . swing. border. TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,
+            12 ), java. awt. Color. red) ,panel1. getBorder( )) ); panel1. addPropertyChangeListener (new java. beans
+            . PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order" .equals (e .
+            getPropertyName () )) throw new RuntimeException( ); }} );
 
             //---- pasueButton ----
             pasueButton.setText("\u5f00\u59cb");
